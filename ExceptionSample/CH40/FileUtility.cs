d7e9cc72535d92f98de1b28884ef2f767182196a
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 16:25:08 +0800

    Fileutility 類別有一個移動檔案的 MoveFiles 函數

---
+++ ExceptionSample/CH40/FileUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExceptionSample.CH40
+{
+    public class FileUtility
+    {
+        public void MoveFiles(string srcFolder, string destFolder)
+        {
+            try
+            {
+                // 複製 srcFolder 所有檔案到 destFolder
+                // 可能發生 IOException
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                // 釋放資源
+            }
+        }
+    }
+}
