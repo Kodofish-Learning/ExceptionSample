9dc8db931f4c9689517723703c3f1d9303889942
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 16:40:13 +0800

    在 try, catch, finally 執行相對應的程式

--- ExceptionSample/CH40/FileUtility.cs
+++ ExceptionSample/CH40/FileUtility.cs
@@ -9,17 +9,20 @@ namespace ExceptionSample.CH40
             var folderCheckPoint = new FolderCheckPoint();
             try
             {
+                folderCheckPoint.Establish(srcFolder);
                 // 複製 srcFolder 所有檔案到 destFolder
                 // 可能發生 IOException
             }
             catch (Exception e)
             {
+                folderCheckPoint.Restore();
                 Console.WriteLine(e);
                 throw;
             }
             finally
             {
                 // 釋放資源
+                folderCheckPoint.Drop();
             }
         }
     }
