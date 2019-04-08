830ad5723bae3586918f7d9b118b1df34d5b57a9
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 14:54:50 +0800

    Install package and prepare Account class and unit test

---
+++ ExceptionSample/CH35/Account.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ErrorHandlerSample.CH35
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public void Withdraw_Amount_biggest_than_balance_should_get_errorCode()
+        {
+            var account = new Account();
+            var actual = account.Withdraw(10);
+            var expected = -1m;
+
+            actual.ShouldBe(expected);
+        }
+
+
+        [Test]
+        public void Withdraw_Amount_smallest_than_balance_should_get_balance()
+        {
+            var account = new Account(100);
+            var actual = account.Withdraw(10);
+            var expected = 90m;
+
+            actual.ShouldBe(expected);
+        }
+
+    }
+    class Account
+    {
+        private decimal _balance;
+
+        public Account():this(0)
+        {
+        }
+
+        public Account(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            if (amount > this._balance)
+            {
+                return -1;
+            }
+            else
+            {
+                this._balance = this._balance - amount;
+                return this._balance;
+            }
+        }
+    }
+}
