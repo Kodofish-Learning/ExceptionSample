8da1006dfd14981c86de98e0cd83a7380039c43d
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 16:30:38 +0800

    實作逐一實作與測試每一個函數

--- ExceptionSample/CH40/FolderCheckPoint.cs
+++ ExceptionSample/CH40/FolderCheckPoint.cs
@@ -3,12 +3,18 @@
     public class FolderCheckPoint
     {
         public void Establish(string backupFolder)
-        { }
+        {
+            // 備份 backupFolder 所包含的全部檔案，包含子目錄的檔案
+        }
 
         public void Restore()
-        { }
+        {
+            // 將備份資料還原
+        }
 
         public void Drop()
-        { }
+        {
+            // 刪除所有備份資料
+        }
     }
 }
