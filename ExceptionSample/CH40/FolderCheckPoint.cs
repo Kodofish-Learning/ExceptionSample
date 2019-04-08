d123cf2a2b85fa70fa7d2b68aebfdb549d5a593d
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 16:28:26 +0800

    引入 CheckPoint

---
+++ ExceptionSample/CH40/FolderCheckPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace ExceptionSample.CH40
+{
+    public class FolderCheckPoint
+    {
+        public void Establish(string backupFolder)
+        { }
+
+        public void Restore()
+        { }
+
+        public void Drop()
+        { }
+    }
+}
