289952e12b6251552cca2880cb30199951696925
Yu Wei Chang <yuwei.chang@xuenn.com>
2019-04-08 19:19:46 +0800

    Add CH14 sample

---
+++ ExceptionSample/CH14/MyStream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionSample.CH14
+{
+    internal class MyStream: MemoryStream
+    {
+        public override void Close()
+        {
+            throw new Exception("close failure");
+        }
+    }
+}
---
+++ ExceptionSample/CH14/OverrideException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace ExceptionSample.CH14
+{
+    [TestFixture]
+    class OverrideException
+    {
+        [Test]
+        public void Main()
+        {
+            try
+            {
+                FinallyoverridesTryException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            } 
+        }
+
+        private void FinallyoverridesTryException()
+        {
+            var ms = new MyStream();
+            try
+            {
+                throw new IOException("Function Failure");
+            }
+            finally
+            {
+                try
+                {
+                    ms.Close();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
